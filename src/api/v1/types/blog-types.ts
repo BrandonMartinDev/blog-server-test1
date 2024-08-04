2fd7a18634d16fdee5f562a12bc6bced55cc512a
Brandon Martin <SomeBusinessEmail@protonmail.com>
2024-08-04 15:25:32 -0400

    Made author type string or User

--- src/api/v1/types/blog-types.ts
+++ src/api/v1/types/blog-types.ts
@@ -1,12 +1,15 @@
 // -- == [[ TYPES ]] == -- \\
 
+import { User } from "./user-types";
+
+
 // Blog Article
 
 type Article = {
 
     _id: string;                        // Handled by MongoDB (BlogID)
 
-    author: string;                     // UserID of the author or author info
+    author: string | User;              // UserID of the author or author info
     createdAt: Date;                    // Date timestamp of when the article was created
 
     title: string;                      // Title of the blog article (min 3 chars, max 100 chars)
